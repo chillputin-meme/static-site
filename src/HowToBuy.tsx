d73ee1206c6fc925dbff3769d21ff0210f27ca28
abtv23 <aniboteva11@gmail.com>
2024-11-23 22:19:24 +0200

    pump fun link

--- src/HowToBuy.tsx
+++ src/HowToBuy.tsx
@@ -22,7 +22,7 @@ const HowToBuy = () => {
                 <div className="how-to-buy-card">
                     <h3>Swap for $chillputin</h3>
                     <p>
-                        Visit <a href="https://pump.fun" target="_blank" rel="noopener noreferrer">pump.fun</a>, paste the contract address (CA), or follow the link to swap your SOL for $chillputin.
+                        Visit <a href="https://pump.fun/coin/F647zieYq5Gt4tVFX8JfJMig1AXKZQ8zCciiJmWFpump" target="_blank" rel="noopener noreferrer">pump.fun</a>, paste the contract address (CA), or follow the link to swap your SOL for $chillputin.
                     </p>
                 </div>
             </div>
